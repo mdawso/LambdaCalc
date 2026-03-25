b975f3f9753d865b0cdb6e809ab6ae11ffacae2b
mdawso <dawsonmathew08@gmail.com>
2026-03-25 21:20:19 +0000

    Added AsString on record types

--- LambdaCalc.cs
+++ LambdaCalc.cs
@@ -2,53 +2,56 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
+using System.Reflection.Metadata;
 using System.Text;
 
 namespace LambdaCalc
 {
-    public abstract record Expression;
-    public record Variable(string name) : Expression;
-    public record Function(string parameter, Expression body) : Expression;
-    public record Application(Expression function, Expression argument) : Expression;
+    public abstract record Expression
+    {
+        public abstract string AsString();
+    }
+    public record Variable(string name) : Expression
+    {
+        public override string AsString() => name;
+    }
+    public record Function(string parameter, Expression body) : Expression
+    {
+        public override string AsString() => $"\\{parameter}.{body.AsString()}";
+    }
+    public record Application(Expression function, Expression argument) : Expression
+    {
+        public override string AsString() => $"({function.AsString()}) {argument.AsString()}";
+    }
     static class LambdaCalc
     {
-        public static string ExpressionToString(Expression expr)
-        {
-            return expr switch
-            {
-                Variable v => $"{v.name}",
-                Function f => $"\\{f.parameter}.{ExpressionToString(f.body)}",
-                Application a => $"({ExpressionToString(a.function)}){ExpressionToString(a.argument)}",
-                _ => throw new ArgumentException("Unknown expression type")
-            };
-        }
-        public static Expression Substitute(Expression expr, string target, Expression replacement)
+        public static Expression BetaReduce(Expression expr, string target, Expression replacement)
         {
             return expr switch
             {
                 Variable v => (v.name == target) ? replacement : v,
-                Function f => (f.parameter == target) ? f : f with { body = Substitute(f.body, target, replacement) },
+                Function f => (f.parameter == target) ? f : f with { body = BetaReduce(f.body, target, replacement) },
                 Application a => new Application(
-                    Substitute(a.function, target, replacement),
-                    Substitute(a.argument, target, replacement)
+                    BetaReduce(a.function, target, replacement),
+                    BetaReduce(a.argument, target, replacement)
                     ),
                 _ => throw new ArgumentException("Unknown expression type")
             };
         }
-
-        public static Expression Reduce(Expression expr)
+        public static Expression Evaluate(Expression expr)
         {
             if (expr is Application app)
             {
-                var func = Reduce(app.function);
-                var arg = Reduce(app.argument);
+                var func = Evaluate(app.function);
+                var arg = Evaluate(app.argument);
                 if (func is Function f)
                 {
-                    return Reduce(Substitute(f.body, f.parameter, arg));
+                    return Evaluate(BetaReduce(f.body, f.parameter, arg));
                 }
                 return new Application(func, arg);
             }
             return expr;
         }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,8 +4,4 @@ using static LambdaCalc.LambdaCalc;
 Function identity = new("I", new Variable("I"));
 Variable v = new("V");
 
-Application a = new(identity, v);
-
-Console.WriteLine(ExpressionToString(a));
-
-Console.WriteLine(ExpressionToString(Reduce(a)));
+Expression a = new Application(identity, new Application(identity, v));
